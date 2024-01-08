6c34e090c8a4b5223d3cd5e8a62a7689d8891d96
material-JH <126924889+material-JH@users.noreply.github.com>
2024-01-08 20:18:18 +0900

    Update setup.py
    c99

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
 from setuptools import Extension, setup
-
-ext_modules = [Extension("chgnet.graph.cygraph", ["chgnet/graph/cygraph.pyx"])]
-
+ext_modules = [Extension("chgnet.graph.cygraph", ["chgnet/graph/cygraph.pyx"], extra_compile_args=["-std=c99"])]
 setup(ext_modules=ext_modules, setup_requires=["Cython"])
